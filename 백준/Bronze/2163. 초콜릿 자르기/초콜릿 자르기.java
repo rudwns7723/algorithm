2147616d4d9a2a1908407119d47b75105ab531f4
rudwns7723 <70430725+rudwns7723@users.noreply.github.com>
2023-11-09 14:36:06 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        System.out.println(num1 * num2 - 1);
+        
+    }
+}
