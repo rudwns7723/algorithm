ef4873d73205c821d4a57990fee4e1a6ce130395
rudwns7723 <70430725+rudwns7723@users.noreply.github.com>
2023-11-09 15:04:31 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -4,17 +4,15 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String arr[] = new String[5];
-        int max = 0;//제일긴 문자열 길이
         
         StringBuilder sb = new StringBuilder();
         
         for(int i=0; i<5; i++){
             arr[i] = br.readLine();
-            if(arr[i].length() > max)max = arr[i].length();
         }
         
         
-        for(int i=0; i<max; i++){
+        for(int i=0; i<15; i++){
             for(int j=0; j<5; j++){
                 if(arr[j].length() > i){
                     sb.append(arr[j].charAt(i));   
