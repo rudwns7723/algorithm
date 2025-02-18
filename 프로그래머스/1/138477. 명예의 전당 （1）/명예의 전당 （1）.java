9038d64c78c5a8430cf5c729a8824b51245e0a3e
rudwns7723 <70430725+rudwns7723@users.noreply.github.com>
2025-02-18 20:24:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.77 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+       int[] answer = new int[score.length];
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for(int i = 0; i < score.length; i++) {
+            q.add(score[i]);
+            
+            if (q.size() > k) {
+                q.poll();
+            }
+
+            answer[i] = q.peek();
+        }
+        return answer;
+    }
+}
